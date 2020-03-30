6ef4cf680d7f34781a1122b51a7fbe1fd21559c4
cyaah <calvin.yah@gmail.com>
2020-03-30 00:05:41 -0400

    fixed up search routes

--- server/controller/portfolioController.js
+++ server/controller/portfolioController.js
@@ -4,3 +4,8 @@ const axios = require('axios');
 exports.getStocks = function(req, res, next){
 res.status(200).send('test')
 }
+
+
+exports.getBalance = function(req, res, next){
+  res.status(200).send('test balance')
+  }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const portfolioController = require('../controller/portfolioController');
+const searchController = require('../controller/searchController');
 
 
 
 //Routes
-router.get('/stocks', portfolioController.getStocks)
+router.get('/stocks', portfolioController.getStocks);
+router.get('/balance', portfolioController.getBalance);
+router.post('/search/stock', searchController.getStockInfo)
+router.post('/search/timeseries', searchController.getTimeSeries)
+
 
 module.exports = router;
